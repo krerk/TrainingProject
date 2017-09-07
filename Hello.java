82115cc62cf45e9b680895da51802b54b6210365
MOTIF\varinthorn.s <varinthorn.st@gmail.com>
2017-09-07 17:24:27 +0700

    no message

--- Hello.java
+++ Hello.java
@@ -9,6 +9,11 @@ class Hello {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
     }
+
+	    private void Varinthorns() {
+        System.out.println("Trainee - Varinthorn Soithong, ASA");
+        System.out.println("Hello Trainer --- ");
+    }
     
     public static void main(String [] argv) {
         new Hello();
