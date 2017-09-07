c5d44d36a185f5ebf2539fab2eb290295a6efd41
thayanee <baiyoke.tyn@gmail.com>
2017-09-07 17:26:02 +0700

    no message

--- Hello.java
+++ Hello.java
@@ -10,6 +10,11 @@ class Hello {
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
     }
     
+	private void thayanee() {
+        System.out.println("Thaynee Ngampitaksin");
+        System.out.println("0940594974");
+    }
+	
     public static void main(String [] argv) {
         new Hello();
     }
